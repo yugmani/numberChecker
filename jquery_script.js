263ee82b2acabfc4eb52723f143cfc60717218c5
Himalaya <yugmani@yahoo.com>
2020-07-22 11:17:30 -0700

    removed some functions

--- jquery_script.js
+++ jquery_script.js
@@ -1,55 +1,69 @@
 // **********************************************************************************
-           // WITH JQUERY  
-           
-           $(document).ready(function() {
-           
-           // Here we create the on click event that gets the user's pick
-          // Then determine which button was clicked
-          // Compare the computer and user guess
-          // Inform the user if the've guessed correctly or incorrectly
-                let select;
-                let sms ="";
-                let btnEl = $(".btn-choice");
-                let random;
-                let html = "";
-            const generateRandom = ()=>{
-                random = Math.floor(Math.random()*4)+1;
-                console.log(random);
-            }
+// WITH JQUERY  
+
+$(document).ready(function() {
+
+// Here we create the on click event that gets the user's pick
+// Then determine which button was clicked
+// Compare the computer and user guess
+// Inform the user if the've guessed correctly or incorrectly
+let select;
+let sms ="";
+let btnEl = $(".btn-choice");
+let random;
+let html = "";
+
+
+const generateRandom = ()=>{
+    select = "";
+    sms ="";
+    //  btnEl = $(".btn-choice");
+    random = "";
+    html = "";
+
+    random = Math.floor(Math.random()*4)+1;
+    $("#picked-number").html("");
+    
+}
                 
-            const pickNumber = ()=>{
-                generateRandom();
-                for (let i=0; i<btnEl.length; i++){
-                    $(btnEl[i]).on("click", (event)=>{
-                        event.preventDefault();
-                      select = $(btnEl[i]).val();
-                      html += `<h6>${select}</h6>`;
-                      $("#picked-number").html(html);
-                    })
-                  }
-                  displayResult(random, select);  
+const playGame = ()=>{
+    
+    generateRandom();
+    $("#computer-pick").text("???");
+    for (let i=0; i<btnEl.length; i++){
+        $(btnEl[i]).on("click", (event)=>{
+            event.preventDefault();
+
+            select = $(btnEl[i]).val();
+            html += `<h5>${select}</h5>`;
+            $("#picked-number").html(html);
+            displayResult(random, select); 
+        });
+        } 
+    }
+ 
+const displayResult = (rand, chosen)=> {
+    if (rand == chosen){
+        sms = `<h2>WINNER!!!</h2>`;
+        sms += `<h1>GAME OVER</h1>`;
+        $("#computer-pick").text(rand);
+        $("#result").html(sms);
+        
+        }
+    
+    else {
+                sms = `<h3>TRY AGAIN</h3>`;
             }
-            
 
-                const displayResult = (rand, chosen)=> {
-                    if (rand == chosen){
-                        sms = "WINNER!!!";
-                        $("#computer-pick").text(rand);
-                      }
-                      else {
-                        sms = "TRY AGAIN";
-                        
-                      }
-  
-                      
-                      
-                      $("#result").text(sms);
+    $("#result").html(sms);
 
-                }
+}
 
-                
-                pickNumber();
+// Start the game;
+playGame();
 
+              
+           
 
-            });
+});
          
